f80f0eeed2839158fd57f0ff2f7683f2687e31c5
Genesis Canela <genesiscanela@Genesiss-MBP.home>
2020-06-16 13:49:04 -0400

    updated to address feedback comments on remainderOf

--- exercises/numbers/remainderOf/remainderOf.js
+++ exercises/numbers/remainderOf/remainderOf.js
@@ -22,16 +22,16 @@ function remainderOf(n, d) {
     If you're not sure, step out of the code and use pen + paper. Start
     with simple examples and pay attention to the process you carry out.
   */
-  return console.log(n % d)
+  return n % d
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for remainderOf');
-  remainderOf(10, 1); 
-  remainderOf(10,2);
-  remainderOf(10, 3); 
-  remainderOf(10, 4);
-  remainderOf(129, 17); 
+  console.log(remainderOf(10, 1)); 
+  console.log(remainderOf(10,2));
+  console.log(remainderOf(10, 3)); 
+  console.log(remainderOf(10, 4));
+  console.log(remainderOf(129, 17)); 
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
