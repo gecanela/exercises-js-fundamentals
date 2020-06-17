128eb25a3806e9e9e6c16b7888cd696f50abdec6
Genesis Canela <genesiscanela@Genesiss-MBP.home>
2020-06-17 00:26:21 -0400

    first draft of longestWord

--- exercises/strings/longestWord/longestWord.js
+++ exercises/strings/longestWord/longestWord.js
@@ -9,10 +9,21 @@
 
 function longestWord(string) {
   // This is your job. :)
+
+  var str = string.split(' ')
+  var findLongest = 0; //variable that stores the length of the longest word as it goes through the loop 
+  for (var i = 0; i < str.length; i++){
+    if(str[i].length > findLongest){
+      findLongest = str[i].length
+      word= str[i]
+    }
+  }
+  return word
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for longestWord:');
+  console.log(longestWord('You are a rockstar'))
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
